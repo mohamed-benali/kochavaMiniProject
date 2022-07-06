cbfef0f034df57ebe22028ca73d4f2cf31e4807b
Mohamed Benali <cg3mbenali@gmail.com>
2022-07-07 01:31:37 +0200

    Add logging + Add calculate deliver elapsed time

--- go/main.go
+++ go/main.go
@@ -3,11 +3,12 @@ package main
 import (
 	"context"
 	"encoding/json"
-	"fmt"
 	"github.com/segmentio/kafka-go"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"net/url"
+	"os"
 	"regexp"
 	"strings"
 	"time"
@@ -20,25 +21,39 @@ const (
 	DEFAULT_UNMATCHED_PARAMETER = "UNMATCHED"
 	RETRY_ATTEMPTS              = 3
 	RETRY_WAIT_TIME             = 1 // In seconds
+	LOGS_ACTIVED                = true
+	LOGS_FILE_NAME              = "logs_file"
 )
 
+func setLogger() {
+	if LOGS_ACTIVED { // If logs are active, it will write them on the file
+		f, err := os.OpenFile(LOGS_FILE_NAME, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+		if err != nil {
+			log.Fatalf("error opening file: %v", err)
+		}
+		log.SetOutput(f)
+	} else {
+		log.SetOutput(ioutil.Discard)
+	}
+}
+
 /**
-Returns the filled URL and the http method from the http body
+Returns the filled URL and the http method using the http body
 */
 func fillURLParameters(body []byte) (method string, urlStr string) {
 	var bodyMap map[string]any
 	error := json.Unmarshal(body, &bodyMap)
 
 	if error != nil {
-		fmt.Println(error)
+		log.Println(error)
 	}
 
 	endpoint := bodyMap["endpoint"].(map[string]any)
 	urlStr = endpoint["url"].(string)
 	method = endpoint["method"].(string)
 
-	data := bodyMap["data"].(map[string]any)
-	for key, value := range data { // For each given query parameter replace it with the given data value
+	data := bodyMap["data"].(map[string]any) // I assume there is not an empty postback
+	for key, value := range data {           // For each given query parameter replace it with the given data value
 		valueToReplace := "{" + key + "}"
 		nReplacements := -1 // -1 because replace all
 		urlStr = strings.Replace(urlStr, valueToReplace, url.QueryEscape(value.(string)), nReplacements)
@@ -49,68 +64,70 @@ func fillURLParameters(body []byte) (method string, urlStr string) {
 	return method, urlStr
 }
 
+// Replaces the non matched query parameters with replace string
 func replaceDefault(input string, replace string) string {
 	sampleRegexp := regexp.MustCompile(`{.*}`) // Regex to match all substrings between '{' and '}' included
 	result := sampleRegexp.ReplaceAllString(input, replace)
 	return result
 }
 
-func sendRequest(method string, url string) bool {
-	client := &http.Client{}
-	req, err := http.NewRequest(method, url, nil)
+// Make the HTTP request and log response data
+func sendRequest(method string, url string) (success bool, res *http.Response) {
+	log.Println("Send request: ", method, " ", url)
+	start := time.Now()
 
+	client := &http.Client{}
+	req, err := http.NewRequest(method, url, nil) // Create request
 	if err != nil {
-		fmt.Println(err)
-		return false
+		log.Println(err)
+		return false, nil
 	}
 
-	res, err := client.Do(req)
+	res, err = client.Do(req) // Send request
 	if err != nil {
-		fmt.Println(err)
-		return false
+		log.Println(err)
+		return false, nil
 	}
 	defer res.Body.Close()
 
-	body, err := ioutil.ReadAll(res.Body)
+	body, err := ioutil.ReadAll(res.Body) // Get response body
 	if err != nil {
-		fmt.Println(err)
-		return false
+		log.Println(err)
+		return false, nil
 	}
-	fmt.Println(string(body))
-	return true
+
+	deliveryTime := time.Since(start)
+
+	log.Println("Response log - Code: ", res.StatusCode, " - Delivery time: ", deliveryTime, " - Body: ", string(body))
+	return true, res
 }
 
 func consume() {
-	r := kafka.NewReader(kafka.ReaderConfig{
+	r := kafka.NewReader(kafka.ReaderConfig{ // Create Apache Kafka Consumer
 		Brokers: []string{brokerAddress},
 		GroupID: GroupID,
 		Topic:   topic})
 	i := 0
 	for { // Infinite loop
-		// the `ReadMessage` method blocks until we receive the next event
-		msg, err := r.ReadMessage(context.Background())
+		msg, err := r.ReadMessage(context.Background()) // the `ReadMessage` method blocks until we receive the next event
 		if err != nil {
-			panic("could not read message " + err.Error())
+			log.Println("could not read message " + err.Error())
 		}
-		// after receiving the message, log its value
 
 		method, urlString := fillURLParameters(msg.Value)
 
-		successDeliver := false
-		deliverTries := 0
+		successDeliver, _ := sendRequest(method, urlString)
+		deliverTries := 1
 		for !successDeliver && deliverTries < RETRY_ATTEMPTS { // Retry if not delivered and less than RETRY_ATTEMPTS tries
-			deliverTries++
-			successDeliver = sendRequest(method, urlString)
+			successDeliver, _ = sendRequest(method, urlString)
 			time.Sleep(RETRY_WAIT_TIME * time.Second)
+			deliverTries++
 		}
-
-		fmt.Println("received: ", string(msg.Value))
-		fmt.Println(i, "\n")
 		i++
 	}
 }
 
 func main() {
-	fmt.Println("Hello, 世界")
+	setLogger()
 	consume()
 }
