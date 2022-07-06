979aebe0da9bda44e057c81b5b1adb70f9dc5464
Mohamed Benali <cg3mbenali@gmail.com>
2022-07-07 01:48:23 +0200

    Add documentation + make broker address a constant

--- php/ingest.php
+++ php/ingest.php
@@ -1,37 +1,26 @@
 <?php
 require 'vendor/autoload.php';
-/* POST Request example
- {
-	"endpoint":{
-		"method":"GET",
-		"url":"http://sample_domain_endpoint.com/data?title={mascot}&image={location}&foo={bar}"
-	},
-	"data":[
-		{
-		"mascot":"Gopher",
-		"location":"https://blog.golang.org/gopher/gopher.png"
-		}
-	]
-}
- */
+
+const BROKER_ADDRESS = '127.0.0.1:9092'; // Apache kafka address
 
 $body = json_decode(file_get_contents('php://input'), true); // Get body as a JSON
 $data = $body["data"];
 
 $method = $body["endpoint"]["method"];
 
-$config = \Kafka\ProducerConfig::getInstance();
+$config = \Kafka\ProducerConfig::getInstance(); // Connect with Kafka as producer
 $config->setMetadataRefreshIntervalMs(10000);
-$config->setMetadataBrokerList('127.0.0.1:9092');
+$config->setMetadataBrokerList(BROKER_ADDRESS);
 $config->setBrokerVersion('1.0.0');
 $config->setRequiredAck(1);
 $config->setIsAsyn(false);
 $config->setProduceInterval(500);
 $producer = new \Kafka\Producer();
 
+// I assume there is at least one postback (count($data) >= 1)
 for($i = 0; $i < count($data); $i++) { // Send postback for each element on the data array
     $payload = $body;
-    $payload["data"] = $data[$i];
+    $payload["data"] = $data[$i]; // Each postback is sent as one object
     $payload = json_encode($payload);
 
     $producer->send([
@@ -43,4 +32,4 @@ for($i = 0; $i < count($data); $i++) { // Send postback for each element on the 
     ]);
 }
 
-echo $method; // Testing that it works
+echo $method; // Writes the method
